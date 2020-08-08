b524b8202e566ef6fadcb769a9314d1d0a6b7e17
Abdallah Daknache <abdallah_daknache@hotmail.com>
2020-08-08 12:59:59 +0300

    Access-token.interface.ts renamed

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post, Req, UseGuards, ValidationPipe } from '@nestjs/common';
 
-import { AccessToken } from '../token/accesstoken.interface';
+import { AccessToken } from '../token/access-token.interface';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
